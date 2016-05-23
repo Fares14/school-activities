5e22888d2027cb6f70ab366d99596aa1083218f0
fares14 <fares.elshawaf@mihsnyc.org>
2016-05-23 13:54:45 +0000

    we did css and javascript

---
+++ js.js
@@ -0,0 +1,34 @@
+var teacher= [ {
+    name: "Nelly", 
+    subject: "Math", 
+    
+},
+   { name: "Athena",
+      subject: "Science" 
+      
+   },
+   {
+       name: "Brian",
+       subject: "Math"
+   },
+   {
+       name: "Matthew",
+       subject: "English"
+       
+   },{
+       name: "geoffroy",
+       subject: "History"
+   }]; 
+   function showteachers(){
+       var radioValue = $("input[name='subject']:checked"). val();
+       console.log(radioValue)
+       
+       
+     for (var i =0; i<teacher.length; i++ ){
+        if () 
+         
+     }
+       }
+  $("#click-me").on("click",showteachers)
+  
+    
